c49918035995c1f5e101cba521780b9fa4b1ac42
EElainaya <eelaina@qq.com>
2024-08-27 12:25:22 +0800

    补充编译所需依赖库

--- build.rs
+++ build.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("cargo:rustc-link-search=./lib")
+    match env::var("TARGET") {
+        Ok(target)=>{
+            match target.as_str() {
+                "aarch64-unknown-linux-gnu"=>{println!("cargo:rustc-link-search=./lib/aarch64")}
+                "x86_64-pc-windows-gnu"=>{println!("cargo:rustc-link-search=./lib/x86_64")}
+                _=>{}
+            }
+        }
+        Err(_)=>{}
+    }    
 }
