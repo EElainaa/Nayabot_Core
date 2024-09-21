d633b5e6b73bb7b1f65be2818aafe4fb41b0f53a
EElainaya <eelaina@qq.com>
2024-09-21 17:31:52 +0800

    update

--- src/bot.rs
+++ src/bot.rs
@@ -1,31 +1,77 @@
 //onebot v11
-use std::{fmt::Display, net::TcpStream, sync::{Arc, Mutex}};
-use crate::{event::*, funs::printinf};
+use std::{collections::HashMap, fmt::Display, net::TcpStream, sync::{Arc, Mutex}, thread::{self, Thread}};
+use crate::{event::*, funs::{printerr, printinf, printwrm}};
+use serde_json::Value;
 use tungstenite::{stream::MaybeTlsStream, Message, WebSocket};
 
 type WS=Arc<Mutex<WebSocket<MaybeTlsStream<TcpStream>>>>;
 
-pub struct BotHttp{}
-pub struct BotRHrrp{}
+//pub struct BotHttp{}
+//pub struct BotRHttp{}
+//pub struct BotRWebsocket{}
 
-#[derive(Debug)]
+pub trait EventResolve {
+    fn run(self,event:Event);
+}
+
+type LifeCycleEventResolve = fn(event:LifecycleEvent);
+type HeartbeatEventResolve = fn(event:HeartbeatEvent);
+type GroupMsgEventResolve = fn(event:GroupMsgEvent);
+type PrivateMsgEventResolve = fn(event:PrivateMsgEvent);
+impl EventResolve for LifeCycleEventResolve {
+    fn run(self,event:Event) {
+        match event.get_lifecycle_event() {
+            Ok(event)=>{self(event)}
+            Err(err)=>{printerr(err)}
+        }
+    }
+}
+impl EventResolve for HeartbeatEventResolve {
+    fn run(self,event:Event) {
+        match event.get_heartbeat_event() {
+            Ok(event)=>{self(event)}
+            Err(err)=>{printerr(err)}
+        }
+    }
+}
+impl EventResolve for GroupMsgEventResolve {
+    fn run(self,event:Event) {
+        match event.get_group_msg_event() {
+            Ok(event)=>{self(event)}
+            Err(err)=>{printerr(err)}
+        }
+    }
+}
+impl EventResolve for PrivateMsgEventResolve {
+    fn run(self,event:Event) {
+        match event.get_private_msg_event() {
+            Ok(event)=>{self(event)}
+            Err(err)=>{printerr(err)}
+        }
+    }
+}
+
+
 pub struct BotWebsocket{
     url:String,
     id:i64,
-    ws:WS
+    ws:WS,
+    subscribes:HashMap<String,fn(Event)>
 }
 
-impl BotWebsocket {
+impl BotWebsocket{
+    ///通过正向ws连接协议端
     pub fn new<T:Display>(url:T)->Result<BotWebsocket,String>{
         match tungstenite::client::connect(url.to_string()) {
             Ok((mut ws,_resp)) =>{
                 match ws.read() {
                     Ok(s)=>{
-                        let lifecycle_event:lifecycle_event=serde_json::from_str(s.to_string().as_str()).unwrap();
+                        let lifecycle_event:LifecycleEvent=serde_json::from_str(s.to_string().as_str()).unwrap();
                         return Ok(BotWebsocket{
                             url:url.to_string(),
                             id: lifecycle_event.self_id,
-                            ws: Arc::new(Mutex::new(ws))
+                            ws: Arc::new(Mutex::new(ws)),
+                            subscribes:HashMap::new()
                         })
                     }
                     Err(err)=>{
@@ -39,28 +85,127 @@ impl BotWebsocket {
     pub fn get_ws(&self) -> WS {
         self.ws.clone()
     }
+    pub fn get_url(&self) -> String{
+        self.url.clone()
+    }
+    pub fn get_id(&self) -> i64{
+        self.id
+    }
 }
 
-trait BotSend {
-    fn send(&mut self,string:String)->Result<(),String>;
-    fn send_with_recive(&mut self,string:String)->Result<String,String>;
-    async fn send_async(&mut self,string:String)->Result<(),String>;
-    async fn send_with_recive_async(&mut self,string:String)->Result<String,String>;
+#[allow(async_fn_in_trait)]
+pub trait Bot{
+    /// 启动一个新线程运行bot
+    fn run(self) -> Thread;
+    //fn run_async(self) -> AbortHandle;
+    /// 发送消息
+    fn send(&mut self,string:&String)->Result<(),String>;
+    /// 发送消息并接收返回消息
+    fn send_with_recive(&mut self,string:&String)->Result<String,String>;
+    /// 异步发送消息
+    async fn send_async(&mut self,string:&String)->Result<(),String>;
+    /// 异步发送消息并接收返回消息
+    async fn send_with_recive_async(&mut self,string:&String)->Result<String,String>;
+    /// 接收消息
+    fn recv_msg(&self)->Result<String,String>;
+    /// 订阅一个事件
+    /// 
+    /// lifecycle_event 生命周期事件
+    /// 
+    /// heartbeat_event 心跳事件
+    /// 
+    /// group_msg_event 群聊消息事件
+    /// 
+    /// private_msg_event 私聊消息事件
+    /// 
+    fn subscribe<T:ToString>(&mut self,t:T,factory:fn(Event))->();
 }
 
-impl BotSend for BotWebsocket {
-    fn send(&mut self,string:String)->Result<(),String> {
+impl Bot for BotWebsocket {
+
+    fn run(self) -> Thread {
+        let bot = self;
+        let handle = thread::spawn(move ||{
+            loop{
+                match bot.recv_msg(){
+                    Ok(str) => {
+                        let d = serde_json::from_str::<Value>(&str.as_str()).unwrap();
+                        match d.get("post_type").unwrap().to_string().as_str() {
+                            "\"meta_event\""=>{
+                                if let Some(f) = bot.subscribes.get("meta_event") {
+                                    //f()
+                                }else {
+                                    match d.get("meta_event_type").unwrap().to_string().as_str(){
+                                        "\"heartbeat\""=>{
+                                            if let Some(f) = bot.subscribes.get("heartbeat_event") {
+                                                let event:HeartbeatEvent = serde_json::from_str(&str).unwrap();
+                                                f(Event::HeartbeatEvent { event })
+                                            }
+                                        }
+                                        _=>{printwrm(format!("未知meta_event事件 {}",str))}
+                                    }
+                                }
+                            }
+                            "\"message\""=>{
+                                if let Some(f) = bot.subscribes.get("message") {
+                                    //f()
+                                }else {
+                                    match d.get("message_type").unwrap().to_string().as_str(){
+                                        "\"group\""=>{
+                                            if let Some(f) = bot.subscribes.get("group_msg_event") {
+                                                let event:GroupMsgEvent = serde_json::from_str(&str).unwrap();
+                                                f(Event::GroupMsgEvent { event })
+                                            }
+                                        }
+                                        "\"private\""=>{
+                                            if let Some(f) = bot.subscribes.get("private_msg_event") {
+                                                let event:GroupMsgEvent = serde_json::from_str(&str).unwrap();
+                                                f(Event::GroupMsgEvent { event })
+                                            }
+                                        }
+                                        _=>{printwrm(format!("未知meta_event事件 {}",str))}
+                                    }
+                                }
+                            }
+                            _=>{printwrm(format!("未知事件 {}",str))}
+                        }
+                        //bot.subscribes[]
+                    },
+                    Err(err) => printerr(err),
+                }
+            }
+        });
+        handle.thread().clone()
+    }
+
+    /*async fn run_async(self) -> AbortHandle {
+        let bot = self;
+        let handle = tokio::spawn(async move ||{
+            loop{
+                match bot.recv_msg(){
+                    Ok(_) => todo!(),
+                    Err(_) => todo!(),
+                }
+            }
+        });
+        handle.await;
+        handle.abort_handle()
+    }*/
+
+    fn send(&mut self,string:&String)->Result<(),String> {
         match self.get_ws().lock().unwrap().send(Message::text(string)){
-            Ok(_)=>{printinf("success");
+            Ok(_)=>{
+                printinf(format!("Send:{}",&string));
                 Ok(())
             }
             Err(err)=>{Err(err.to_string())}
         }
     }
     
-    fn send_with_recive(&mut self,string:String)->Result<String,String> {
+    fn send_with_recive(&mut self,string:&String)->Result<String,String> {
         match self.send(string){
             Ok(_)=>{
+                printinf(format!("Send:{}",&string));
                 match self.ws.lock().unwrap().read() {
                     Ok(s) => {
                         Ok(s.to_string())
@@ -72,14 +217,14 @@ impl BotSend for BotWebsocket {
         }
     }
     
-    async fn send_async(&mut self,string:String)->Result<(),String> {
+    async fn send_async(&mut self,string:&String)->Result<(),String> {
         match self.ws.lock().unwrap().send(Message::text(string)){
             Ok(_)=>{Ok(())}
             Err(err)=>{Err(err.to_string())}
         }
     }
     
-    async fn send_with_recive_async(&mut self,string:String)->Result<String,String> {
+    async fn send_with_recive_async(&mut self,string:&String)->Result<String,String> {
         match self.ws.lock().unwrap().send(Message::text(string)){
             Ok(_)=>{
                 match self.ws.lock().unwrap().read() {
@@ -93,28 +238,36 @@ impl BotSend for BotWebsocket {
             Err(err)=>{Err(err.to_string())}
         }
     }
-}
 
+    fn recv_msg(&self)->Result<String,String>{
+        Ok(self.get_ws().lock().unwrap().read().unwrap().to_string())
+    }
+    fn subscribe<T:ToString>(&mut self,t:T,factory:fn(Event))->() {
+        self.subscribes.insert(t.to_string(), factory);
+    }
+}
 impl BotWebsocket {
+    ///发送私聊消息
     pub fn send_private_msg<T:Display>(&mut self,id:&i64,s:T)->Result<(),String>{
         send_private_msg(self, id, s)
     }
-    pub fn get_status(&mut self)->Result<echo_get_status, String>{
+    ///获取登录状态
+    pub fn get_status(&mut self)->Result<EchoGetStatus, String>{
         get_status(self)
     }
-    pub fn get_version_info(&mut self)->Result<echo_get_version_info, String>{
+    ///获取协议端版本信息
+    pub fn get_version_info(&mut self)->Result<EchoGetVersionInfo, String>{
         get_version_info(self)
     }
+    ///获取登录账号信息
+    pub fn get_login_info(&mut self)->Result<EchoLoginInfo, String>{
+        get_login_info(self)
+    }
 }
-
-pub struct BotRWebsocket{}
 
 /*
 #[allow(dead_code)]
 impl Bot {
-    pub fn send_private_msg<T:Display>(&self,id:&i64,s:T)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"send_private_msg\",\"params\": {{\"user_id\":{},\"message\":{}}}}}",id,s))}
-    pub fn send_group_msg<T:Display>(&self,id:&i64,s:T)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"send_group_msg\",\"params\": {{\"group_id\":{},\"message\":{}}}}}",id,s))}
-    pub fn delete_msg(&self,msg_id:&i64)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"delete_msg\",\"params\": {{\"message_id\":{}}}}}",msg_id))}
     pub fn get_msg(&self,msg_id:&i64)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"get_msg\",\"params\": {{\"message_id\":{}}}}}",msg_id))}
     pub fn get_forward_msg(&self,id:&i64)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"get_forward_msg\",\"params\": {{\"id\":{}}}}}",id))}
     pub fn send_like(&self,user_id:&i64,times:i16)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"send_like\",\"params\": {{\"user_id\":{},\"times\":{}}}}}",user_id,times))}
@@ -150,29 +303,46 @@ impl Bot {
     pub fn set_restart(&self,delay:&i64)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"set_restart\",\"params\": {{\"delay\":{}}}}}",delay))}
     pub fn clean_cache(&self)->Result<(), ws::Error>{self.sender.send(format!("{{\"action\": \"clean_cache\"}}"))}
 }*/
-
-fn send_private_msg<B:BotSend,T:Display>(bot:&mut B,id:&i64,s:T)->Result<(), String>{
-    return bot.send(format!("{{\"action\": \"send_private_msg\",\"params\": {{\"user_id\":{},\"message\":{}}}}}",id,s))
+fn send_private_msg<B:Bot,T:Display>(bot:&mut B,id:&i64,s:T)->Result<(), String>{
+    bot.send(&format!("{{\"action\": \"send_private_msg\",\"params\": {{\"user_id\":{},\"message\":{}}}}}",id,s))
 }
 
-//获取bot状态
-fn get_status<T:BotSend>(bot:&mut T)->Result<echo_get_status, String>{
-    match bot.send_with_recive(format!("{{\"action\": \"get_status\"}}")){
+fn send_group_msg<B:Bot,T:Display>(bot:&mut B,id:&i64,s:T)->Result<(), String>{
+    bot.send(&format!("{{\"action\": \"send_group_msg\",\"params\": {{\"group_id\":{},\"message\":{}}}}}",id,s))
+}
+
+fn delete_msg<B:Bot>(bot:&mut B,msg_id:&i64)->Result<(), String>{
+    bot.send(&format!("{{\"action\": \"delete_msg\",\"params\": {{\"message_id\":{}}}}}",msg_id))
+}
+
+fn get_msg<B:Bot>(bot:&mut B,msg_id:&i64)->Result<(), String>{
+    bot.send(&format!("{{\"action\": \"get_msg\",\"params\": {{\"message_id\":{}}}}}",msg_id))
+}
+
+fn get_status<T:Bot>(bot:&mut T)->Result<EchoGetStatus, String>{
+    match bot.send_with_recive(&format!("{{\"action\": \"get_status\"}}")){
         Ok(res)=>{
-            printinf("send successly");
-            Ok(serde_json::from_value::<echo_get_status>(serde_json::from_str::<echo_event>(res.as_str()).unwrap().data).unwrap())
+            Ok(serde_json::from_value::<EchoGetStatus>(serde_json::from_str::<EchoEvent>(res.as_str()).unwrap().data).unwrap())
         }
         Err(err)=>{Err(err)}
     }
 }
 
-fn get_version_info<T:BotSend>(bot:&mut T)->Result<echo_get_version_info, String>{
-    match bot.send_with_recive(format!("{{\"action\": \"get_version_info\"}}")){
+fn get_version_info<T:Bot>(bot:&mut T)->Result<EchoGetVersionInfo, String>{
+    match bot.send_with_recive(&format!("{{\"action\": \"get_version_info\"}}")){
         Ok(res)=>{
-            Ok(serde_json::from_value::<echo_get_version_info>(serde_json::from_str::<echo_event>(res.as_str()).unwrap().data).unwrap())
+            Ok(serde_json::from_value::<EchoGetVersionInfo>(serde_json::from_str::<EchoEvent>(res.as_str()).unwrap().data).unwrap())
         }
         Err(err)=>{Err(err)}
     }
 }
 
+fn get_login_info<T:Bot>(bot:&mut T)->Result<EchoLoginInfo, String>{
+    match bot.send_with_recive(&format!("{{\"action\": \"get_login_info\"}}")){
+        Ok(res)=>{
+            Ok(serde_json::from_value::<EchoLoginInfo>(serde_json::from_str::<EchoEvent>(res.as_str()).unwrap().data).unwrap())
+        }
+        Err(err)=>{Err(err)}
+    }
+}
 
--- src/event.rs
+++ src/event.rs
@@ -1,5 +1,5 @@
 
-use std::fmt::{self};
+use std::fmt;
 
 use colored::Colorize;
 use serde::{Deserialize, Serialize};
@@ -7,9 +7,43 @@ use serde_json::Value;
 
 use crate::funs::{array_to_string, time_to_string};
 
-#[allow(non_camel_case_types)]
+#[derive(Debug)]
+pub enum Event {
+    LifecycleEvent{event:LifecycleEvent},
+    HeartbeatEvent{event:HeartbeatEvent},
+    GroupMsgEvent{event:GroupMsgEvent},
+    PrivateMsgEvent{event:PrivateMsgEvent}
+}
+
+impl Event {
+    pub fn get_lifecycle_event(self) -> Result<LifecycleEvent,String>{
+        match self {
+            Event::LifecycleEvent{event} =>return  Ok(event),
+            _=>{return Err("生命周期事件转换失败".to_string());}
+        }
+    }
+    pub fn get_heartbeat_event(self) -> Result<HeartbeatEvent,String>{
+        match self {
+            Event::HeartbeatEvent{event} =>return  Ok(event),
+            _=>{return Err("心跳周期事件转换失败".to_string());}
+        }
+    }
+    pub fn get_group_msg_event(self) -> Result<GroupMsgEvent,String>{
+        match self {
+            Event::GroupMsgEvent{event} =>return  Ok(event),
+            _=>{return Err("群聊消息事件转换失败".to_string());}
+        }
+    }
+    pub fn get_private_msg_event(self) -> Result<PrivateMsgEvent,String>{
+        match self {
+            Event::PrivateMsgEvent{event} =>return  Ok(event),
+            _=>{return Err("私聊消息事件转换失败".to_string());}
+        }
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-pub struct lifecycle_event{//生命周期
+pub struct LifecycleEvent{//生命周期
     pub time:i64,
     pub self_id:i64,
     pub post_type:String,
@@ -17,15 +51,8 @@ pub struct lifecycle_event{//生命周期
     pub sub_type:String
 }
 
-impl fmt::Display for lifecycle_event {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,"{} [{}]成功连接Bot{}",time_to_string(self.time),"Inf".green(),self.self_id.to_string().blue())
-    }
-}
-
-#[allow(non_camel_case_types)]
 #[derive(Debug, Serialize, Deserialize)]
-pub struct heartbeat_event{//心跳
+pub struct HeartbeatEvent{//心跳
     pub time:i64,
     pub self_id:i64,
     pub post_type:String,
@@ -34,9 +61,8 @@ pub struct heartbeat_event{//心跳
     pub interval:i32
 }
 
-#[allow(non_camel_case_types)]
 #[derive(Debug, Serialize, Deserialize)]
-pub struct group_msg_event{
+pub struct GroupMsgEvent{
     pub self_id:i64,
     pub user_id:i64,
     pub time:i64,
@@ -54,15 +80,14 @@ pub struct group_msg_event{
     pub group_id:i64
 }
 
-impl fmt::Display for group_msg_event {
+impl fmt::Display for GroupMsgEvent {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f,"{} [{}][{}]{}[{: >11}] | {}({}):{} ",time_to_string(self.time),"Inf".green(),self.self_id.to_string().blue(),"群".purple(),self.group_id.to_string().purple(),self.sender["nickname"],self.user_id,array_to_string(self.message.as_array().unwrap()))
     }
 }
 
-#[allow(non_camel_case_types)]
 #[derive(Debug, Serialize, Deserialize)]
-pub struct private_msg_event{
+pub struct PrivateMsgEvent{
     pub self_id:i64,
     pub user_id:i64,
     pub time:i64,
@@ -79,15 +104,14 @@ pub struct private_msg_event{
     pub post_type:String
 }
 
-impl fmt::Display for private_msg_event {
+impl fmt::Display for PrivateMsgEvent {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f,"{} [{}][{}]{}[{: >11}] | {}({}):{} ",time_to_string(self.time),"Inf".green(),self.self_id.to_string().blue(),"私".purple(),self.user_id.to_string().purple(),self.sender["nickname"],self.user_id,array_to_string(self.message.as_array().unwrap()))
     }
 }
 
-#[allow(non_camel_case_types)]
 #[derive(Debug, Serialize, Deserialize)]
-pub struct echo_event{
+pub struct EchoEvent{
     pub status:String,
     pub retcode:i8,
     pub data:Value,
@@ -96,19 +120,23 @@ pub struct echo_event{
     pub echo:String
 }
 
-#[allow(non_camel_case_types)]
 #[derive(Debug, Serialize, Deserialize)]
-pub struct echo_get_status{
+pub struct EchoGetStatus{
     pub online:bool,
     pub good:bool,
     pub stat:Value
 }
 
-#[allow(non_camel_case_types)]
 #[derive(Debug, Serialize, Deserialize)]
-pub struct echo_get_version_info{
+pub struct EchoGetVersionInfo{
     pub app_name:String,
     pub protocol_version:String,
     pub app_version:String
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EchoLoginInfo{
+    pub user_id:i64,
+    pub nickname:String
 }
 
